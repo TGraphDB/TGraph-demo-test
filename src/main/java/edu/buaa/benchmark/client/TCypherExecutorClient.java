c69df88913a7cf1cb94484be9a0cb129c877e737
sjh <songjignhe@163.com>
2020-11-27 10:20:01 +0800

    begin tcypher client/server test

--- src/main/java/edu/buaa/benchmark/client/TCypherExecutorClient.java
+++ src/main/java/edu/buaa/benchmark/client/TCypherExecutorClient.java
@@ -1,14 +1,20 @@
 package edu.buaa.benchmark.client;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import edu.buaa.benchmark.transaction.AbstractTransaction;
-import edu.buaa.benchmark.transaction.ImportTemporalDataTx;
+import edu.buaa.benchmark.transaction.*;
+import edu.buaa.benchmark.transaction.index.CreateTGraphAggrDurationIndexTx;
+import edu.buaa.benchmark.transaction.index.CreateTGraphAggrMaxIndexTx;
+import edu.buaa.benchmark.transaction.index.CreateTGraphTemporalValueIndexTx;
+import edu.buaa.benchmark.transaction.internal.EarliestArriveTimeAggrTx;
 import edu.buaa.client.TGraphSocketClient;
 import edu.buaa.model.StatusUpdate;
 import edu.buaa.utils.TimeMonitor;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TCypherExecutorClient extends TGraphSocketClient implements DBProxy {
     public TCypherExecutorClient(String serverHost, int parallelCnt, int queueLength) throws IOException{
@@ -27,20 +33,95 @@ public class TCypherExecutorClient extends TGraphSocketClient implements DBProxy
 
     @Override
     public ListenableFuture<ServerResponse> execute(AbstractTransaction tx) throws Exception {
-        return null;
+        switch (tx.getTxType()){
+            case tx_import_static_data: return execute((ImportStaticDataTx) tx);
+            case tx_import_temporal_data: return execute((ImportTemporalDataTx) tx);
+            case tx_query_snapshot_aggr_max: return execute((SnapshotAggrMaxTx) tx);
+            case tx_query_snapshot_aggr_duration: return execute((SnapshotAggrDurationTx) tx);
+            case tx_query_snapshot: return execute((SnapshotQueryTx) tx);
+            case tx_query_road_by_temporal_condition: return execute((EntityTemporalConditionTx) tx);
+            case tx_index_tgraph_aggr_max: return execute((CreateTGraphAggrMaxIndexTx) tx);
+            case tx_index_tgraph_aggr_duration: return execute((CreateTGraphAggrDurationIndexTx) tx);
+            case tx_index_tgraph_temporal_condition: return execute((CreateTGraphTemporalValueIndexTx) tx);
+            default:
+                throw new UnsupportedOperationException();
+        }
     }
 
-    private String importTemporalData(ImportTemporalDataTx tx){
+    private ListenableFuture<ServerResponse> execute(ImportStaticDataTx tx){
+        StringBuilder sb = new StringBuilder();
+        for (ImportStaticDataTx.StaticCrossNode p : tx.getCrosses()) {
+            sb.append(MessageFormat.format("CREATE ({name: '{0}', tid: {1} });", p.getName(), p.getId()));
+        }
+        for (ImportStaticDataTx.StaticRoadRel sr : tx.getRoads()) {
+            sb.append(MessageFormat.format("CREATE (n)-[r:ROAD_TO {name: '{2}'}]->(m) WHERE id(n)={0} AND id(m)={1}", sr.getStartCrossId(), sr.getEndCrossId(), sr.getId()));
+        }
+        return addQuery(sb.toString());
+    }
+
+    private ListenableFuture<ServerResponse> execute(ImportTemporalDataTx tx){
         StringBuilder sb = new StringBuilder();
         for (StatusUpdate s : tx.data) {
-            sb.append(MessageFormat.format("MATCH ()-[r:ROAD_TO]->() WHERE id(r)={0} SET " +
+            sb.append(MessageFormat.format("MATCH ()-[r:ROAD_TO]->() WHERE r.name='{0}' SET " +
                     "r.travel_time=TV({1}~NOW:{2}), " +
                     "r.full_status=TV({1}~NOW:{3}), " +
-//                    "r.vehicle_count=TV({1}~NOW:{4}); "
                     "r.segment_count=TV({1}~NOW:{5});"
                     ,s.getRoadId(), s.getTime(), s.getTravelTime(), s.getJamStatus(), s.getSegmentCount()));
         }
-        return sb.toString();
+        return addQuery(sb.toString());
+    }
+
+    private ListenableFuture<ServerResponse> execute(SnapshotAggrMaxTx tx){
+        return addQuery(
+                MessageFormat.format(
+                        "MATCH ()-[r:ROAD_TO]->() RETURN r.name, TAGGR_MAX(r.{0}, {1}~{2})"
+            ,tx.getP(), tx.getT0(), tx.getT1()));
+    }
+
+    private ListenableFuture<ServerResponse> execute(SnapshotAggrDurationTx tx){
+        return addQuery(
+                MessageFormat.format(
+                        "MATCH ()-[r:ROAD_TO]->() RETURN r.name, TAGGR_DURATION(r.{0}, {1}~{2})"
+                        ,tx.getP(), tx.getT0(), tx.getT1()));
+    }
+
+    private ListenableFuture<ServerResponse> execute(SnapshotQueryTx tx){
+        return addQuery(
+                MessageFormat.format(
+                        "MATCH ()-[r:ROAD_TO]->() RETURN r.name, TFV(r.{0}, {1})"
+                        ,tx.getPropertyName(), tx.getTimestamp()));
+    }
+
+    private ListenableFuture<ServerResponse> execute(EntityTemporalConditionTx tx){
+        return addQuery(
+                MessageFormat.format(
+                        "MATCH ()-[r:ROAD_TO]->() WHERE TVRC(r.{0}, any, {1}~{2}, {3}, {4}) RETURN r.name"
+                        ,tx.getP(), tx.getT0(), tx.getT1(), tx.getVmin(), tx.getVmax()));
+    }
+
+    private ListenableFuture<ServerResponse> execute(CreateTGraphAggrMaxIndexTx tx){
+        return addQuery(
+                MessageFormat.format(
+                        "CREATE TEMPORAL INDEX(min_max) ON ({0}) DURING {1}~{2} ARGS({3}, {4})"
+                        ,tx.getProName(), tx.getStart(), tx.getEnd(), tx.getEvery(), tx.getTimeUnit()));
+    }
+
+    private ListenableFuture<ServerResponse> execute(CreateTGraphAggrDurationIndexTx tx){
+        return addQuery(
+                MessageFormat.format(
+                        "CREATE TEMPORAL INDEX(duration) ON ({0}) DURING {1}~{2} ARGS({3}, {4})"
+                        ,tx.getProName(), tx.getStart(), tx.getEnd(), tx.getEvery(), tx.getTimeUnit()));
+    }
+
+    private ListenableFuture<ServerResponse> execute(CreateTGraphTemporalValueIndexTx tx){
+        StringBuilder b = new StringBuilder();
+        for(String p : tx.getProps()){
+            b.append(p).append(',');
+        }
+        return addQuery(
+                MessageFormat.format(
+                        "CREATE TEMPORAL INDEX(value) ON ({0}) DURING {1}~{2}"
+                        ,b.substring(0, b.length()-1), tx.getStart(), tx.getEnd()));
     }
 
     @Override
@@ -61,5 +142,17 @@ public class TCypherExecutorClient extends TGraphSocketClient implements DBProxy
     @Override
     public void close() throws IOException, InterruptedException {
 
+    }
+
+    public static class Result extends AbstractTransaction.Result{
+        Map<String, List<Object>> results = new HashMap<>();
+
+        public Map<String, List<Object>> getResults() {
+            return results;
+        }
+
+        public void setResults(Map<String, List<Object>> results) {
+            this.results = results;
+        }
     }
 }
--- src/main/java/edu/buaa/server/TCypherTcpServer.java
+++ src/main/java/edu/buaa/server/TCypherTcpServer.java
@@ -1,11 +1,17 @@
 package edu.buaa.server;
 
+import edu.buaa.benchmark.client.TCypherExecutorClient;
 import edu.buaa.benchmark.transaction.AbstractTransaction;
 import edu.buaa.utils.TGraphSocketServer;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TCypherTcpServer extends TGraphSocketServer.ReqExecutor{
     public static void main(String[] args){
@@ -30,10 +36,20 @@ public class TCypherTcpServer extends TGraphSocketServer.ReqExecutor{
 
     @Override
     protected AbstractTransaction.Result execute(String line) throws RuntimeException {
+        Map<String, List<Object>> results = new HashMap<>();
         try(Transaction tx = db.beginTx()){
-            db.execute(line);
+            Result r = db.execute(line);
+            while(r.hasNext()){
+                Map<String, Object> row = r.next();
+                row.forEach((k, v)->{
+                    List<Object> list = results.computeIfAbsent(k, k1 -> new ArrayList<>());
+                    list.add(v);
+                });
+            }
             tx.success();
         }
-        return null;
+        TCypherExecutorClient.Result rr = new TCypherExecutorClient.Result();
+        rr.setResults(results);
+        return rr;
     }
 }
