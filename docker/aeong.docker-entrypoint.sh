ad6169c5014efe74981fb2087be2cd16137811ce
songjinghe <songjinghe@users.noreply.github.com>
2026-01-30 22:42:50 +0800

    Create aeong.docker-entrypoint.sh

---
+++ docker/aeong.docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+/home/AeonG/build/memgraph --bolt-port 7687 \
+   --data-directory /database/      \
+   --log-file=/database/aeong.log  \
+   --log-level=DEBUG           \
+   --also-log-to-stderr        \
+   --bolt-server-name-for-init=Neo4j/5.2.0 \
+   --storage-snapshot-on-exit=true         \
+   --data_recovery_on_startup=true         \
+   --storage-snapshot-interval-sec 30      \
+   --storage-gc-cycle-sec 30   \
+   --storage-properties-on-edges=true     \
+   --memory-limit 0            \
+   --anchor-num 10             \
+   --real-time-flag=false
